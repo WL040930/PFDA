75a3b71eec439b2a43f68fb15c4e5fcb57b86f45
WL <limweilun3838@gmail.com>
2025-01-22 20:28:47 +0800

    Group OS into OS_CLEANED

--- Assignment_Script.R
+++ Assignment_Script.R
@@ -82,3 +82,18 @@ print(summary(data_unique))
 cat("\nDuplicate removal process completed successfully.\n")
 
 sum(is.na(data_unique))
+data$OS_Cleaned <- case_when(
+  grepl("Windows|Win|Microsoft", data$OS, ignore.case = TRUE) & !grepl("Phone|Mobile", data$OS, ignore.case = TRUE) ~ "Windows", 
+  grepl("Linux|Ubuntu|Debian|Red Hat|CentOS|Fedora|Mint", data$OS, ignore.case = TRUE) ~ "Linux",  
+  grepl("BSD|FreeBSD|OpenBSD|NetBSD|Tru64|AIX|Solaris|HP-UX|IRIX|Unix", data$OS, ignore.case = TRUE) ~ "Unix",  
+  grepl("MacOS|OS X", data$OS, ignore.case = TRUE) ~ "MacOS",  
+  grepl("Android|iOS|Windows Phone|iPXE|F5|Cisco|Juniper|Netgear|HP", data$OS, ignore.case = TRUE) ~ "Embedded/Other",  
+  TRUE ~ "Others"
+)
+
+cat("Unique values in the cleaned OS field:\n")
+print(unique(data$OS_Cleaned))
+
+os_summary <- table(data$OS_Cleaned)
+cat("\nSummary of OS categories:\n")
+print(os_summary)
